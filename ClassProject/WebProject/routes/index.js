c188c65c234449415c5d7697879cf8f9a6afa1d0
Hetvi Patel <Hetvi.patel9@ontariotechu.net>
2024-11-05 22:11:50 -0500

    express part 5 helfway

--- ClassProject/WebProject/routes/index.js
+++ ClassProject/WebProject/routes/index.js
@@ -3,7 +3,38 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Home'
+  });
 });
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { 
+    title: 'Home'
+  });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { 
+    title: 'About'
+  });
+});
+
+/* GET Projects page. */
+router.get('/Projects', function(req, res, next) {
+  res.render('index', { 
+    title: 'Projects'
+  });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { 
+    title: 'Contact Us'
+  });
+});
+
 
 module.exports = router;
